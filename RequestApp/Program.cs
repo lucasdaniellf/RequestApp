2b03e74e340d468f0386a28c3fb770e56b33c593
lucas <lucas@DESKTOP-B9BKUMM>
2021-12-14 20:17:42 -0300

    Adicionar arquivos de projeto.

---
+++ RequestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+
+
+namespace RequestApp
+{
+    public class MainClass
+    {
+        static string url
+        {
+            get { return "https://jsonplaceholder.typicode.com"; }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Request request = new Request();
+            Console.WriteLine("Making Requests");
+
+            await request.MakeRequest(url + "/todos");
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
---
+++ RequestApp/Request.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestApp
+{
+    public class Request
+    {
+        public async Task MakeRequest(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var result = JsonConvert.DeserializeObject<Model[]>(content);
+
+                    if (result?.Length > 0)
+                    {
+                        foreach (Model model in result)
+                        {
+                            Console.WriteLine($"{model.Id} - {model.Title}: {model.Completed}");
+                        }
+
+                    }
+                }
+
+                client.Dispose(); 
+            }
+        }
+    }
+}
